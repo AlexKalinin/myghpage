ed864be12511f49a7adfd4541ae80735be35f3c8
Ivan Lyubchenko <lubchenko2044@gmail.com>
2021-07-28 09:14:16 +0300

    f

--- extension-script.js
+++ extension-script.js
@@ -9,20 +9,32 @@ $('header').append(input);
 
 
 input.onchange = () => {
-  let fd = new FormData();
-  fd.append('file', input.files[0]);
-
+  
   let url = `https://gitlab.com/nt24.pro/personal/ivan_lyubchenko/gitlab-attacments/uploads`;
 
-  let req = $.ajax({
-    beforeSend: function(request) {
-      request.setRequestHeader("x-csrf-token", csrf);
-    },
-    url: url,
-    type: 'POST',
-    processData: false,
-    data: {file: fd}
-  });
+  // let req = $.ajax({
+  //   beforeSend: function(request) {
+  //     request.setRequestHeader("x-csrf-token", csrf);
+  //   },
+  //   url: url,
+  //   type: 'POST',
+  //   processData: false,
+  //   data: {file: fd}
+  // });
+  let file = input.files[0];
+
+
+  let fr = new FileReader();
+  fr.onloadend = e => {
+    let arrayBuffer = e.target.result;
+    let fileType = file.type;
+    var blob = blobUtil.arrayBufferToBlob(arrayBuffer, fileType);
+    console.log('here is a blob', blob);
+    console.log('its size is', blob.size);
+    console.log('its type is', blob.type);
+  };
+
+  fr.readAsArrayBuffer(file);
 };
 
 console.log('hethere');
